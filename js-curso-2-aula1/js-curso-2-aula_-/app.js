0da9ec5f51e0834ff1a0ffdc973494fd29aae0bd
Thiago Moraes <thimoraesadm@gmail.com>
2024-03-27 03:04:08 -0300

    first commit

---
+++ js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('.tituloJogo');
+titulo.innerHTML = 'Jogo do numero secreto';
+
+let paragrafro = document.querySelector('.texto__paragrafo');
+paragrafro.innerHTML = 'Escolha um numero de 1 a 20';
